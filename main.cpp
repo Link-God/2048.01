c2eaa077a0ff400142f2b608e6aa82693b643412
Link-God <32190920+Link-God@users.noreply.github.com>
2017-10-16 16:00:03 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include"stdafx.h"
+static bool prov_0 = false;
+static bool lose = false;
 void sort_down(int a[4][4])
 {
 	for (int j = 0; j < 4; j++)
@@ -29,7 +31,7 @@ void sort_up(int a[4][4])
 		{
 			if (a[i][j] == 0)
 			{
-				for (int k = i+1; k < 4; k++)
+				for (int k = i + 1; k < 4; k++)
 				{
 					if (a[k][j] != 0)
 					{
@@ -158,66 +160,118 @@ void left_1(int a[4][4])
 }
 
 #include <iostream>
+#include <iomanip>h
 using namespace std;
-void print (int a[4][4])
+
+void print(int a[4][4])
 {
 	for (int i = 0; i < 4; i++)
 	{
 		cout << endl;
 		for (int j = 0; j < 4; j++)
 		{
-			if (a[i][j] == 0) cout << "*";
+			if (a[i][j] == 0) cout<< setw(4) << "*" ; 
 			else
 			{
-				if (a[i][j] > 10 && j != 0) cout << " " << a[i][j]; // для чисел больше 10 пробел , для простоты восприятия 
-				else cout << a[i][j];
+				 cout << setw(4) << a[i][j] ; 
 			}
 		}
 	}
+	cout << endl;
 }
 
-#include <iostream>
-using namespace std;
-void read(int a[4][4])
+void new_null(int a[4][4])
 {
 	for (int i = 0; i < 4; i++)
 	{
 		for (int j = 0; j < 4; j++)
 		{
-			cin >> a[i][j];
+			a[i][j] = 0;
 		}
+	}
+}
+
+bool proverka(int a[4][4])
+{
+	int k=0;
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 4; j++)
+		{
+			if (a[i][j] == 0)  k += 1;
+		}
+	}
+	if (k == 0)
+	{
+		prov_0 = true;
+		return true;
+	}
+	else return false;
+}
+
+#include <iostream>
+using namespace std;
+int rand_1(int a[4][4])
+{
+	int i, j;
+	i = rand() % 4;
+	j = rand() % 4;
+	if (proverka(a) && prov_0)
+	{
+		cout << "lose" << endl;
+		lose = true;
+		system("pause");
+		return -1;
+	}
+	else 
+	{
+		if (a[i][j] != 0) rand_1(a);
+		else
+		{
+			int ran = rand() % 10;
+			if (ran < 8) a[i][j] = 2;
+			else a[i][j] = 4;
+		}
+		return 1;
 	}
 }
 
 #include <iostream>
 using namespace std;
-int sdvig( int a[4][4])
+int sdvig(int a[4][4])
 {
-	char op ;
+	char op;
 	while (true)
 	{
-		cin.get();
+	//	cin.get(); переход на другую строку сделал в print
 		cin >> op;
 		switch (op)
 		{
 		case 'j':
 			down(a);
-			print(a);
+			rand_1(a);
+			print(a) ;
+			if (lose) return -1;
 			cout << endl;
 			break;
 		case 'k':
 			up(a);
+			rand_1(a);
 			print(a);
+			if (lose) return -1;
 			cout << endl;
 			break;
 		case 'h':
 			left_1(a);
+			rand_1(a);
 			print(a);
 			cout << endl;
 			break;
 		case 'l':
 			right_1(a);
+			rand_1(a);
 			print(a);
+			if (lose) return -1;
 			cout << endl;
 			break;
 		case 'q':
@@ -230,10 +284,15 @@ int sdvig( int a[4][4])
 }
 
 #include <iostream>
+#include<ctime>
+#include <cstdlib>
 int main()
 {
 	int matrix[4][4];
-	read(matrix);
+	srand(time(0));
+	new_null(matrix);
+	rand_1(matrix); rand_1(matrix); rand_1(matrix);
+	print(matrix);
 	sdvig(matrix);
 	return 0;
 }
